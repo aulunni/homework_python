913ac2e05fb5667c6c44829b6e655c23eb4d27b0
Xenia <89976203+aulunni@users.noreply.github.com>
2021-09-19 16:26:57 +0300

    Update 0909_2.py

--- 0909_2.py
+++ 0909_2.py
@@ -1,5 +1,19 @@
 import random
-my_list = ['Sergey' , 'Julia' , 'Egor' , 'Igor' , 'Evelin' , 'German' , 'Bagrat' , 'Regina' , 'Bogdan' , 'Leonid' , 'Xenia' , 'Leonard' , 'Ivan' , 'Elena']
-random.shuffle(my_list)
-i = my_list.pop()
-print(i)
+
+vowel=['a','e','y','u','i','o']
+cons=['q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+ln=['Chen','Brantford','Doll','Fire']
+
+def generation_name(vowel,cons,num):
+    i = 1
+    while i < num :
+        name = ''
+        name += random.choice(ln) + ' '
+        for x in range(0, 3):
+            name += random.choice(cons)
+            name += random.choice(vowel)
+
+        yield name
+        i += 1
+for name in generation_name(vowel,cons,5):
+    print(name)
